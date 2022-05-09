68b1d986abe3471b32917bcac2a3b7ed54bd3240
OliverSu <oliversu15@outlook.com>
2022-05-09 21:21:02 +0800

    finiish basic RespondObject

---
+++ srpc/common/RespondObject.h
@@ -0,0 +1,74 @@
+#ifndef RESPOND_OBJECT_H
+#define RESPOND_OBJECT_H
+#include <json.hpp>
+#include <string>
+
+#include "RequestObject.h"
+namespace srpc {
+namespace common {
+static const std::string json_result_stirng = "result";
+static const std::string json_error_stirng = "error";
+static const std::string json_code_stirng = "code";
+static const std::string json_message_stirng = "message";
+static const std::string json_data_stirng = "data";
+
+class ErrorObject {
+ public:
+  ErrorObject(int code, const std::string& message) {
+    // TODO valid code
+    _object.add(json_code_stirng, code);
+    _object.add(json_message_stirng, message);
+  }
+  ErrorObject(int code, const std::string& message, const JsonData& data)
+      : ErrorObject(code, message) {
+    _object.add(json_data_stirng, data);
+  }
+  ErrorObject(int code, const std::string& message, const JsonArray& data)
+      : ErrorObject(code, message) {
+    _object.add(json_data_stirng, data);
+  }
+  ErrorObject(int code, const std::string& message, const JsonObject& data)
+      : ErrorObject(code, message) {
+    _object.add(json_data_stirng, data);
+  }
+
+  JsonObject& get_json_object() { return _object; }
+
+ private:
+  JsonObject _object;
+};
+
+class RespondObject {
+ public:
+  RespondObject(const JsonData& result, int id) : RespondObject(id) {
+    _object.add(json_result_stirng, result);
+  }
+  RespondObject(const JsonArray& result, int id) : RespondObject(id) {
+    _object.add(json_result_stirng, result);
+  }
+  RespondObject(const JsonObject& result, int id) : RespondObject(id) {
+    _object.add(json_result_stirng, result);
+  }
+
+  RespondObject(ErrorObject& error, int id) : RespondObject(id) {
+    _object.add(json_error_stirng, error.get_json_object());
+  }
+
+ private:
+  RespondObject() {
+    init_head();
+    _object.add(json_id_stirng, nullptr);
+  };
+  RespondObject(int id) {
+    init_head();
+    _object.add(json_id_stirng, id);
+  };
+  void init_head() { _object.add(json_rpc_stirng, json_rpc_v2_stirng); }
+
+  JsonObject _object;
+  bool _error;
+};
+}  // namespace common
+}  // namespace srpc
+
+#endif
